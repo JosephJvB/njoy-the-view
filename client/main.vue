7ddd00e62f171f81457e108f6b7d4d5fe45be1ae
JosephJvB <joevanbo@pm.me>
2018-09-14 16:32:22 +1200

    prog(main): plan out components in main.vue

--- client/main.vue
+++ client/main.vue
@@ -1,11 +1,42 @@
+// ---TEMPLATE---
 <template>
-  <p>sup world Im {{ message ? message : 'stressin' }}</p>
+  <div>
+    <p>hey world</p>
+    <!-- <Header></Header>
+    <VideoCarousel v-bind:videos="newVideos"></VideoCarousel>
+    <WatchedVideosList v-bind:videos="watchedVideos"></WatchedVideosList> -->
+  </div>
 </template>
+
+// ---SCRIPT---
 <script>
-  // imports here
+  // import components here
+  // import Header from './components/Header.vue'
+  // import VideoCarousel from './components/Video-Carousel.vue'
+  // import WatchedVideosList from './components/Watched-Videos-List.vue'
+
   export default {
+    name: 'Main',
+    components: {
+      // Header,
+      // VideoCarousel,
+      // WatchedVideosList,
+    },
     data: function () {
-      return {message: 'happy'}
+      return { 
+        newVideos: [],
+        watchedVideos: [],
+      }
+    },
+    methods: {
+      fetchVideos: function () {},
+      listVideoAsWatched: function () {},
+    },
+    mounted: function () {
     },
   }
 </script>
+
+// ---STYLE---
+<style>
+</style>
